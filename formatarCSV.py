49ea5d3b6d2632c3e8b590a03c0494bbecd5801d
ataias <elioenay99@gmail.com>
2024-02-16 15:52:46 -0400

    Adição de logs na formatarCSV.py

--- formatarCSV.py
+++ formatarCSV.py
@@ -1,25 +1,37 @@
 import pandas as pd
+import logging
+
+# Configuração do logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def traduzir_e_formatar_csv(arquivo_entrada, arquivo_saida):
-    # Leitura do CSV
-    df = pd.read_csv(arquivo_entrada)
+    logger.info(f"Iniciando a tradução e formatação do arquivo: {arquivo_entrada}")
 
-    # Tradução das Colunas
-    colunas_traduzidas = {
-        'open': 'Abertura',
-        'high': 'Máxima',
-        'low': 'Mínima',
-        'close': 'Fechamento',
-        'volume': 'Volume',
-        '30_day_moving_avg': 'Média de movimentação em 30 dias',
-        'daily_pct_change': 'Movimentação diária'
-    }
-    df.rename(columns=colunas_traduzidas, inplace=True)
+    try:
+        # Leitura do CSV
+        df = pd.read_csv(arquivo_entrada)
+        logger.info("Arquivo CSV lido com sucesso.")
+        # Tradução das Colunas
+        colunas_traduzidas = {
+            'open': 'Abertura',
+            'high': 'Máxima',
+            'low': 'Mínima',
+            'close': 'Fechamento',
+            'volume': 'Volume',
+            '30_day_moving_avg': 'Média de movimentação em 30 dias',
+            'daily_pct_change': 'Movimentação diária'
+        }
+        df.rename(columns=colunas_traduzidas, inplace=True)
+        logger.info("Colunas traduzidas.")
+        # Formatação de volume para o padrão brasileiro
+        df['Volume'] = df['Volume'].apply(
+            lambda x: f"{x:,.2f}".replace(',', 'temp').replace('.', ',').replace('temp', '.'))
+        logger.info("Dados formatados.")
+        # Exportação do CSV traduzido e formatado
+        df.to_csv(arquivo_saida, index=False)
+        logger.info(f"Arquivo CSV traduzido e formatado exportado com sucesso: {arquivo_saida}")
 
-    # Formatação de volume para o padrão brasileiro
-    df['Volume'] = df['Volume'].apply(lambda x: f"{x:,.2f}".replace(',', 'temp').replace('.', ',').replace('temp', '.'))
-
-    # Exportação do CSV traduzido e formatado
-    df.to_csv(arquivo_saida, index=False)
-
+    except Exception as e:
+        logger.error(f"Erro ao traduzir e formatar o arquivo: {e}")
